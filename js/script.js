72f40dcb4c23cb56223313cac05ee274b28c14a1
Liza <goldglaider@gmail.com>
2021-10-19 23:37:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10,
+	decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+// && 'и'
+// || 'или'
+
+
+const isChecked = true,
+		isClose = false;
+
+console.log(isChecked || isClose);
+
+console.log(2 + 2 * 2 !== '6');
