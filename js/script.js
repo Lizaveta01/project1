e773b1f501f9971af8e80cec8fa511a0eb93d199
Liza <goldglaider@gmail.com>
2021-10-22 12:00:03 +0300

    практическое задание 2

--- js/script.js
+++ js/script.js
@@ -94,17 +94,89 @@
 // 	num++;
 // }
 
-let num = 50;
-for (let i = 1; i < 8; i++)
-{ 
-	if (i === 6) 
-	{ 
-	//break
-	continue;
-	}
-	console.log(i);
+// let num = 50;
+// for (let i = 1; i < 8; i++)
+// { 
+// 	if (i === 6) 
+// 	{ 
+// 	//break
+// 	continue;
+// 	}
+// 	console.log(i);
 	
-}
+// }
+
+// Урок 15 
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+// 		count: numberOfFilms,
+// 		movies: {},
+// 		actors: {},
+// 		genres:[],
+// 		privat: false
+// 	};
+	
+
+
+// 	for (let i = 0; i < 2; i++) { 
+// 		const a = prompt('Один из последних просмотренных фильмов', ''),
+// 				b = prompt('На сколько оцените его?', '');
+
+//  Нереализованный вариант
+// switch (a) {
+// 	case null: 
+// 		console.log('неверно!');
+// 		break;
+// 	case '': 
+// 		console.log('неверно!');
+// 		break;
+// 	default: 
+// 	console.log('ок');
+// 	break;
+// } 
+
+// switch (b) {
+// 	case null: 
+// 		console.log('неверно!');
+// 		break;
+// 	case '': 
+// 		console.log('неверно!');
+// 		break;
+// 	default: 
+// 	console.log('ок');
+// 	break;
+// } 
+
+// 	}
+// Вариант 1
+		// if (a != null && b != null && a !='' && b != '' && a.length < 50) {
+		// 	personalMovieDB.movies[a] = b;
+		// 	console.log('done');
+		// }
+		// else {
+		// 	console.log('Error');
+		// 	i--;
+		// }
+			
+	
+
+
+
+// 	if (personalMovieDB.count < 10) {
+// 		console.log("Просмотрено довольно мало фильмов");
+// 	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) 
+// 	{console.log("Вы классический зритель");}
+// 	else if (personalMovieDB.count >= 30) 
+// 	{console.log("Вы киноман");}
+// 	else 
+// 	{console.log("Произошла ошибка");}
+
+	
+
+// console.log(personalMovieDB);
+
 
 
 
